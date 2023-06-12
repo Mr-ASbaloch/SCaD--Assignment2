edcc8cc4acd4a5ebd4d414fa62cd7028fcec2743
abdul <abdulsaeed1356@gmail.com>
2023-06-12 09:02:39 +0500

    work done

---
+++ loop.py
@@ -0,0 +1,22 @@
+
+#  for loop
+
+fruits = ['apple', 'banana', 'orange']
+for fruit in fruits:
+    print(fruit)
+
+# other 
+
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  if x == "banana":
+    continue
+  print(x) 
+
+
+
+# do while loop 
+num = 1
+while num <= 5:
+    print(num)
+    num += 1
