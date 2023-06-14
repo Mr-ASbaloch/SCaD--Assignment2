a9f10c0a3bcd7f5d3f282c2bad24b9fd4c945a30
abdul <abdulsaeed1356@gmail.com>
2023-06-14 11:36:42 +0500

    work done

---
+++ dic.py
@@ -0,0 +1,18 @@
+
+
+
+capital_city = {"Nepal": "Kathmandu", "Italy": "Rome", "England": "London"}
+print(capital_city)
+
+# dictionary with keys and values of different data types
+numbers = {1: "One", 2: "Two", 3: "Three"}
+print(numbers)
+
+
+
+capital_city = {"Nepal": "Kathmandu", "England": "London"}
+print("Initial Dictionary: ",capital_city)
+
+capital_city["Japan"] = "Tokyo"
+
+print("Updated Dictionary: ",capital_city)
--- loop.py
+++ loop.py
@@ -16,7 +16,7 @@ for x in fruits:
 
 
 # do while loop 
-num = 1
-while num <= 5:
-    print(num)
-    num += 1
+# num = 1
+# while num <= 5:
+#     print(num)
+#     num += 1
